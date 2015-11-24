abd47ef4bc802c32abc7d2748d361c1db97bfe3c
李雙雙 <f228300k@gmail.com>
2015-11-24 13:54:55 +0800

    push~

---
+++ src/main/java/tw/com/softleader/e5e5/service/CalculateService.java
@@ -0,0 +1,17 @@
+package tw.com.softleader.e5e5.service;
+
+public class CalculateService {
+	
+	
+	public int sum(int a, int b) {
+		return a+b;
+	}
+	public int mut(int a ,int b){
+		return a*b;
+	}
+	public int min (int a, int b){
+		return a-b;
+	}
+
+
+}
---
+++ src/test/java/tw/com/softleader/e5e5/service/CalculateServiceTest.java
@@ -0,0 +1,38 @@
+package tw.com.softleader.e5e5.service;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class CalculateServiceTest {
+
+	@Test
+	public void testSum() {
+		
+		CalculateService cal = new CalculateService();
+		int sum = cal.sum(2, 3);
+		assertEquals(5, sum);
+		
+	}
+	
+	@Test
+	public void testMut() {
+		CalculateService cal = new CalculateService();
+		int mut = cal.mut(2, 3);
+		cal.mut(2, 3);
+		assertEquals(6, mut);
+	}
+	@Test 
+	public void testMin() {
+		CalculateService cal = new CalculateService();
+		int min = cal.min(3, 2);
+		assertEquals(1, min);
+	}
+	
+	@Test
+	public void testDiv(){
+	CalculateService cal = new CalculateService();
+
+	}
+
+}
